49ebe3fd4751c1ae9b9e2bb442e3f87729a4ac3d
Johan Paredes <jmpareds@gmail.com>
2022-04-24 02:48:00 -0400

    Add files via upload

--- InstructionFetch/main.cpp
+++ InstructionFetch/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 #include <fstream>
 using namespace std;
 
@@ -13,7 +14,18 @@ enum Instructions {
 	SLTU,
 	SLL,
 	SRL,
-	SRA
+	SRA,
+	ADDI,
+	ANDI,
+	ORI,
+	XORI,
+	SLTI,
+	SLTUI,
+	SLLI,
+	SRLI,
+	SRAI,
+	LW,
+	SW
 };
 
 Instructions resolveInstruction(std::string input) {
@@ -23,17 +35,71 @@ Instructions resolveInstruction(std::string input) {
     if( input == "or" )  return OR;
     if( input == "xor" ) return XOR;
     if( input == "slt" ) return SLT;
-    if( input == "sltu" ) return SLTU;
+    if( input == "sltu" )return SLTU;
     if( input == "sll" ) return SLL;
     if( input == "srl" ) return SRL;
     if( input == "sra" ) return SRA;
+    if( input == "addi" ) return ADDI;
+    if( input == "andi" ) return ANDI;
+    if( input == "ori" )  return ORI;
+    if( input == "xori" ) return XORI;
+    if( input == "slti" ) return SLTI;
+    if( input == "sltui" )return SLTUI;
+    if( input == "slli" ) return SLLI;
+    if( input == "srli" ) return SRLI;
+    if( input == "srai" ) return SRAI;
+    if( input == "lw" ) return LW;
+    if( input == "sw" ) return SW;
     return INVALID;
- }
+}
 
- void TypeR(ifstream &input, string &rd, string &rs1, string &rs2, int &opcode, string &instruction){
-	getline(input, rd, ' ');
-	getline(input, rs1, ' ');
-	getline(input, rs2, '\n');
+void TypeR(string &line, string &rd, string &rs1, string &rs2, int &opcode, string &instruction){
+	rd = line.substr(0, line.find(','));
+	line.erase(0, line.find(',')+1);
+
+	rs1 = line.substr(0, line.find(','));
+	line.erase(0, line.find(',')+1);
+
+	rs2 = line;
+
+	opcode = resolveInstruction(instruction);
+}
+
+void TypeI(string &line, string &rd, string &rs1, string &imm, int &opcode, string &instruction){
+	rd = line.substr(0, line.find(','));
+	line.erase(0, line.find(',')+1);
+
+	rs1 = line.substr(0, line.find(','));
+	line.erase(0, line.find(',')+1);
+
+	imm = line;
+
+	opcode = resolveInstruction(instruction);
+}
+
+void TypeL(string &line, string &rd, string &imm, string &rs1, int &opcode, string &instruction){
+	rd = line.substr(0, line.find(','));
+	line.erase(0, line.find(',')+1);
+
+	imm = line.substr(0, line.find('('));
+	line.erase(0, line.find('(')+1);
+
+	rs1 = line.substr(0, line.find(')'));
+	line.erase(0, line.find(')')+1);
+
+	opcode = resolveInstruction(instruction);
+}
+
+void TypeS(string &line, string &rd, string &imm, string &rs1, int &opcode, string &instruction){
+	rd = line.substr(0, line.find(','));
+	line.erase(0, line.find(',')+1);
+
+	imm = line.substr(0, line.find('('));
+	line.erase(0, line.find('(')+1);
+
+	rs1 = line.substr(0, line.find(')'));
+	line.erase(0, line.find(')')+1);
+
 	opcode = resolveInstruction(instruction);
  }
 
@@ -47,14 +113,26 @@ int main(){
 		return 0;
 	}
 
-	int PROGRAM_COUNTER = -1;
+	string instructionMemory[100];
+	int i = 0;
 
 	while(input.peek() != EOF){
-		PROGRAM_COUNTER += 1;
-		string instruction;
-		getline(input, instruction, ' ');
+		getline(input,	instructionMemory[i], '\n');
+		i++;		
+	}
 
-		string rd, rs1, rs2, panic;
+	int PROGRAM_COUNTER = 0;
+	string end = "exit";
+
+	while(instructionMemory[PROGRAM_COUNTER].compare(end) != 0){
+		string line = instructionMemory[PROGRAM_COUNTER];
+
+		string instruction = line.substr(0, line.find(' '));
+		line.erase(0, line.find(' ')+1);
+
+		line.erase(remove(line.begin(), line.end(), ' '), line.end());
+
+		string rd, rs1, rs2;
 		int opcode = -1;
 
 		switch(resolveInstruction(instruction)){
@@ -68,16 +146,32 @@ int main(){
 			case SLL:
 			case SRL:
 			case SRA:
-				TypeR(input, rd, rs1, rs2, opcode, instruction);
+				TypeR(line, rd, rs1, rs2, opcode, instruction);
+			break;
+			case ADDI:
+			case ANDI:
+			case ORI:
+			case XORI:
+			case SLTI:
+			case SLTUI:
+			case SLLI:
+			case SRLI:
+			case SRAI:
+				TypeI(line, rd, rs1, rs2, opcode, instruction);
+			break;
+			case LW:
+				TypeL(line, rd, rs1, rs2, opcode, instruction);
+			break;
+			case SW:
+				TypeS(line, rd, rs1, rs2, opcode, instruction);
 			break;
 			default:
-				getline(input, panic, '\n');
-				cout << instruction << " es una instruccion invalida \n";
-				continue;
+				cout << "'" << instruction << "' es una instruccion invalida \n";
 			break;
-		}	
+		}
 
 		cout << "'" << PROGRAM_COUNTER << " " << opcode << " " << instruction << " " << rd << " " << rs1 << " " << rs2 << "'" <<'\n';
+		PROGRAM_COUNTER += 1;
 	}
 
 	input.close();
