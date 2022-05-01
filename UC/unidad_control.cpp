b7de5bf69c15ae34cd89982def2c81c64e97694d
juanmpy05 <66576421+juanmpy05@users.noreply.github.com>
2022-04-30 22:26:02 -0400

    Update unidad_control.cpp
    se resuelve el problema de que las señales estaban invertidas.
    
    se corrige un comentario en la linea 68

--- UC/unidad_control.cpp
+++ UC/unidad_control.cpp
@@ -46,8 +46,8 @@ void UnidadControl::obtener_operacion() {
 
 	// es un instruccíon de tipo R
 	if (opcodeIn.read() >= ADD && opcodeIn.read() <= SRA) {
-		ALUSrcOut.write(false);         // el 2do operando de la ALU será el 2do registro leido del AR
-		MemtoRegOut.write(false);       // el valor enviado al AR será la salida del ALU 
+		ALUSrcOut.write(true);         // el 2do operando de la ALU será el 2do registro leido del AR
+		MemtoRegOut.write(true);       // el valor enviado al AR será la salida del ALU 
 		RegWriteOut.write(true);        // se escribe en el registro destino el valor presente en write data (habilita la escritura en AR)
 		MemWriteOut.write(false);       // no se permite la escritura en DataMemory
 		MemReadOut.write(false);        // no se permite la lectura en DataMemory 
@@ -55,8 +55,8 @@ void UnidadControl::obtener_operacion() {
 	 
 	// instruccion de tipo I
 	} else if (opcodeIn.read() >= ADDI && opcodeIn.read() <= SRAI) {
-		ALUSrcOut.write(true);          // el 2do operando de la ALu será el valor inmediato
-		MemtoRegOut.write(false);       // el valor enviado al AR será la salida del ALU
+		ALUSrcOut.write(false);          // el 2do operando de la ALu será el valor inmediato
+		MemtoRegOut.write(true);       // el valor enviado al AR será la salida del ALU
 		RegWriteOut.write(true);        // se escribe en el registro destino el valor presente en write data
 		MemWriteOut.write(false);       // no se permite la escritura en DataMemory
 		MemReadOut.write(false);       // no se permite la lectura en DataMemory
@@ -64,16 +64,16 @@ void UnidadControl::obtener_operacion() {
 
 	// instruccion de tipo L
 	} else if (opcodeIn.read() == LW) {
-		ALUSrcOut.write(true);          // el 2do operando de la ALu será el valor inmediato
-		MemtoRegOut.write(true);        // el valor enviado al AR será la salida del ALU
-		RegWriteOut.write(true);        // se escribe enel registro destino el valor presente en write data
+		ALUSrcOut.write(false);          // el 2do operando de la ALu será el valor inmediato
+		MemtoRegOut.write(false);        // el valor enviado al AR será el que arroje la DM (dataOut) 
+		RegWriteOut.write(true);        // se escribe en el registro destino el valor presente en write data
 		MemWriteOut.write(false);       // no se permite la escritura en DataMemory
 		MemReadOut.write(true);         // permite la lectura en DataMemory
 		branchOut.write(false);         // sin saltos
 
 	// instruccion de tipo S
 	} else if (opcodeIn.read() == SW) {
-		ALUSrcOut.write(true);         // el 2do operando de la ALu será el valor inmediato 
+		ALUSrcOut.write(false);         // el 2do operando de la ALu será el valor inmediato 
 		MemtoRegOut.write(none);       // no se enviará al AR (no importa que valor se envie, no se usará)
 		RegWriteOut.write(false);      // no se permite la escritura en el registro AR
 		MemWriteOut.write(true);       // se permite la escritura en DataMemory
@@ -84,7 +84,7 @@ void UnidadControl::obtener_operacion() {
 	// instruccion de tipo salto
 	} else if (opcodeIn.read() >= BEQ && opcodeIn.read() <= BGE) {
 		cout << "UnidadControl llama un salto \n";
-		ALUSrcOut.write(false);         // el 2do operando de la ALu será el 2do registro leído del AR
+		ALUSrcOut.write(true);         // el 2do operando de la ALu será el 2do registro leído del AR
 		MemtoRegOut.write(none);       // no se enviará al AR (no importa que valor se envie, no se usará)
 		RegWriteOut.write(false);      // no se permite la escritura en el registro AR
 		MemWriteOut.write(false);       // no se permite la escritura en DataMemory
